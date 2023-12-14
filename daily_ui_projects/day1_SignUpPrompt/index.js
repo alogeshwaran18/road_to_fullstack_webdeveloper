7d655586e89efe47de2a5b651bf42d24cfe3289f
alogeshwaran18 <alogeshwaran18@gmail.com>
2023-12-14 08:56:52 +0530

    day-1 finished

---
+++ daily_ui_projects/day1_SignUpPrompt/index.js
@@ -0,0 +1,5 @@
+document.getElementById('signup-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add your signup form submission logic here
+    alert('Sign up logic goes here!');
+});
